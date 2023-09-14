12b6acac0d02d1f67a39ccb88b8703d447da6be9
choidongkuen <danaver12@daum.net>
2023-09-14 22:06:52 +0900

    Spring api gateway 모듈 구현 및 api gateway service 모듈 구현

---
+++ spring_api_gateway/src/main/java/com/example/SpringApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiGatewayApplication.class,args);
+    }
+}
--- userservice/src/main/java/com/example/UserServiceApplication.java
+++ userservice/src/main/java/com/example/UserServiceApplication.java
@@ -1,11 +1,9 @@
-package com.example.userservice;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class,args);
