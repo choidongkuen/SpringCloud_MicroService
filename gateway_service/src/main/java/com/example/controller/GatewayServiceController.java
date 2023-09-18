d4817f88ac6c7e4b04d28ac91db8bba03717e313
choidongkuen <danaver12@daum.net>
2023-09-18 10:54:15 +0900

    spring cloud gateway java code로 적용하기

---
+++ gateway_service/src/main/java/com/example/controller/GatewayServiceController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class GatewayServiceController {
+
+    @GetMapping("/first-service")
+    public String firstService(
+            @RequestHeader("first-service-header") String header){
+        System.out.println("first-service-header : " + header);
+        return "This is a first Service";
+    }
+
+    @GetMapping("/second-service")
+    public String secondService(
+            @RequestHeader("second-service-header") String header) {
+
+        System.out.println("second-service-header : " + header);
+        return "This is a second Service";
+    }
+}
---
+++ spring_api_gateway/src/main/java/com/example/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.example.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Spring Cloud Gateway 을 Java Code 로 적용하기
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-service-header","first value")
+                                .addResponseHeader("first-response","first response value"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-service-header","second value")
+                                .addResponseHeader("second-response","second response value"))
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
