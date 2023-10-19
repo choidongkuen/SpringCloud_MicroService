83669e816d85d1f1a8fcdef6195d4a82c9ac8ce4
choidongkuen <danaver12@daum.net>
2023-10-20 01:33:03 +0900

    ecommerce project - orders-service 설정 파일 - 설정 파일 저장소에 저장 및 config server 관련 설정

--- orders-service/src/main/java/com/example/OrdersServiceApplication.java
+++ orders-service/src/main/java/com/example/OrdersServiceApplication.java
@@ -2,10 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@EnableDiscoveryClient
 @EnableJpaAuditing
-//@EntityScan(basePackages = {"com.example.domain.entity"})
 @SpringBootApplication
 public class OrdersServiceApplication {
     public static void main(String[] args) {
